b3e700fc6ec8fbc4590506e46e979fbd6778df17
oltapllana <olta.pllana@student.uni-pr.edu>
2022-04-11 17:46:24 +0200

    TCP_Klienti

--- TCP_Klienti/TCP_Klienti/Program.cs
+++ TCP_Klienti/TCP_Klienti/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace MultiClient
+{
+    class Program
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 100;
+
+        static void Main()
+        {
+            Console.Title = "Client";
+            ConnectToServer();
+            RequestLoop();
+            Exit();
+        }
+
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+            IPAddress myIP = IPAddress.Parse("127.1.0.1");
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+
+                    ClientSocket.Connect(myIP, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
